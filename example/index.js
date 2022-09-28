557b43c4c0929cd218db174852e2a056475ff996
Guillermo Ruffino <glm.net@gmail.com>
2022-09-27 23:35:51 -0300

    shadow-bug-repro

--- example/index.js
+++ example/index.js
@@ -1,6 +1,29 @@
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
+
+import editorStyles from "monaco-editor/min/vs/editor/editor.main.css";
+
 export const initMonaco = () => {
+  monaco.languages.register({ id: 'mySpecialLanguage' });
+
+  monaco.languages.registerHoverProvider('mySpecialLanguage', {
+    provideHover: function (model, position) {
+
+      return {
+        range: new monaco.Range(
+          1,
+          1,
+          model.getLineCount(),
+          model.getLineMaxColumn(model.getLineCount())
+        ),
+        contents: [
+          { value: '**HOVER WORKING PROPERLY**' },
+        ]
+      };
+    }
+  });
+
+
   const $root = document.getElementById('root');
   monaco.editor.create($root, {
     value: '// type your code...',
@@ -8,8 +31,31 @@ export const initMonaco = () => {
   });
 
   const $yaml = document.getElementById('yaml');
-  monaco.editor.create($yaml, {
-    value: '// type your yaml code...',
-    language: 'yaml',
+
+  const div2 = document.createElement("div");
+  div2.style.height = "300px";
+  editorStyles
+  $yaml.attachShadow({ mode: 'open' });
+
+  $yaml.shadowRoot.appendChild(div2);
+
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.media = 'screen';
+  style.innerHTML = editorStyles;
+
+  $yaml.shadowRoot.appendChild(style);
+
+
+  const editor1 = monaco.editor.create(div2, {
+    value: '\n\nHover over this text',
+    language: 'mySpecialLanguage',
+    automaticLayout: true
+  });
+  setTimeout(1000, () => {
+    editor1.layout({
+      height: 300,
+      width: 300
+    })
   });
 };
